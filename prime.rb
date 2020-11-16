4b161eb5480bea4278e9ba65d614e5b6251129af
Vincent Ivos <vincentivos1@gmail.com>
2020-11-15 23:31:22 -0500

    commit tree 000856a7d6c3ec97a858dfc6d1c2b97cb6279203

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+    first = 2
+    if num > 1
+      r = (first..num-1).to_a
+      r.none? do |item| 
+        num % item == 0
+      end
+    else
+      return false
+    end
+  end # Add  code here!
